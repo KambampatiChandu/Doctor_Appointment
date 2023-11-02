2ac264b3f3d0d961fae73cb6d3f93e61c507faa2
KambampatiChandu <122196852+KambampatiChandu@users.noreply.github.com>
2023-11-02 16:59:51 +0530

    Add files via upload

---
+++ Doctor_Appointment/Doctor_Appointment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Doctor_Appointment
+//
+//  Created by Student on 11/08/1945 Saka.
+//  Copyright © 1945 LPU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
